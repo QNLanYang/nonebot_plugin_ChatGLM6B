0fea4cfd3828697444d131e26dc03ae3a6217bd2
QNLanYang <QNLanYang@outlook.com>
2023-03-21 19:33:02 +0800

    First commit，only basic fuctions

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from . import getchat, config
+
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name="ChatGLM-6B",
+    description="调用本地 ChatGLM API 进行聊天",
+    usage="@机器人 +【命令前缀（如果有）】+ GLM|. +【内容】"
+)
+
